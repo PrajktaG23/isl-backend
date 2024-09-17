f4199c5c21092debcd591ecd39384a8d736e62ef
Prajkta Gaikwad <prajkta07g@gmail.com>
2024-09-17 14:44:21 +0530

    Initial commit

---
+++ src/main/java/com/platform/isl_backend/IslBackendApplication.java
@@ -0,0 +1,13 @@
+package com.platform.isl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/isl_backend/IslBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.isl_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
