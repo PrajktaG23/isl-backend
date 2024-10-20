1015d333620bca440b74ff6b1688faca55d0f652
Prajkta Gaikwad <prajkta07g@gmail.com>
2024-10-20 21:44:52 +0530

    webconfig added

---
+++ src/main/java/com/platform/isl_backend/Config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.platform.isl_backend.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
+    }
+}
