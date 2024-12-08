8ede7d74b74752aafe2fbece6c753230854f10d2
Sumaiya Ansari <122565069+SUMAIYA2921@users.noreply.github.com>
2024-12-08 10:23:20 +0530

    Video formatting and changes in port in web config

--- src/main/java/com/platform/isl_backend/Config/WebConfig.java
+++ src/main/java/com/platform/isl_backend/Config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5501", "http://127.0.0.1:5501", "http://localhost:8080")
+                .allowedOrigins("http://localhost:5500", "http://localhost:5501", "http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:8080", "http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
